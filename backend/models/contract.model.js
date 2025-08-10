457f3b453163df0c385489f144c55097f379d601
VivaCristioRey <chris.jara0626@gmail.com>
2025-08-09 23:17:45 -0500

    Se crea el modelo del esquema de contrato

---
+++ backend/models/contract.model.js
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose
+const contractSchema = new Schema({
+    oferta: {
+        idOferta: {
+            type: Schema.Types.ObjectId,
+            ref: 'offer',
+            required: true
+        },
+        nombreOferta: {
+            type: String,
+            required: true,
+            maxLength: [50, 'El nombre solo acepta 50 caracteres']
+        }
+    },
+
+    empleado: {
+        idUsuario: {
+            type: Schema.Types.ObjectId,
+            ref: 'user',
+            required: true
+        },
+        nombre: {
+            type: String,
+            required: true,
+            trim: true,
+            maxLength: [50, 'El nombre solo acepta 50 caracteres']
+        }
+    },
+
+    empleador: {
+        idUsuario: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user',
+            required: true
+        },
+        nombre: {
+            type: String,
+            required: true,
+            trim: true,
+            maxLength: [50, 'El nombre solo acepta 50 caracteres']
+        }
+    },
+
+    fechaInicio: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+
+    fechaFin: {
+        type: Date,
+        required: true
+    },
+
+    estado: {
+        type: String,
+        required: true,
+        enum: ['Activo', 'Inactivo']
+    }
+})
+
+export default mongoose.model('contract', contractSchema)
