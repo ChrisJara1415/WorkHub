0c145b09d68ca2cb1e1dc7d35515604037785cb1
VivaCristioRey <chris.jara0626@gmail.com>
2025-08-11 23:06:26 -0500

    Se termina el esquema de postulaciones

---
+++ backend/models/applyments.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose
+const applymentSchema = Schema({
+    servicio: {
+        idServicio: {
+            type: Schema.Types.ObjectId,
+            ref: 'ofertas',
+            required: true
+        },
+
+        nombreServicio: {
+            type: String,
+            trim: true,
+            required: true,
+            maxLength: [50, 'El nombre no acepta más de 50 caracteres']
+        }
+    },
+
+    empleado: {
+        idUsuario: {
+            type: Schema.Types.ObjectId,
+            ref: 'usuarios',
+            required: true,
+        },
+        nombre: {
+            type: String,
+            required: true,
+            trim: true,
+            maxLength: [50, 'El nombre no puede exceder los 50 caracteres']
+        }
+    },
+
+    fechaPostulacion: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+
+    estado: {
+        type: String,
+        required: true,
+        enum: ['Aceptada', 'Pendiente', 'Rechazada']
+    }
+})
+
+export default mongoose.model('postulaciones', applymentSchema)
