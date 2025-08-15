c5eb013f70002f7326c8854e5da4c2e6d83fee7a
ChrisJara1415 <chris.jara0626@gmail.com>
2025-08-15 13:18:40 -0500

    Se agrega la variable de entorno PORT y se crea el index para el funcionamiento correcto del backend

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import exp from "express";
+import dotenv from "dotenv";
+import usersRouter from './routers/users.router.js'
+import morgan from "morgan";
+
+dotenv.config()
+
+const app = exp()
+const PORT = process.env.PORT
+
+app.use(exp.json())
+app.use(morgan('dev'))
+app.use('/clientes', usersRouter)
+
+app.listen(process.env.PORT, () => {
+    try {
+        console.log(`Servidor escuchando en el puerto ${PORT}`)
+        console.log(`Servidor disponible en http://localhost:${PORT}`)
+    } catch (error) {
+        console.error('Error al inicializar servidor', error.message)
+    }
+})
