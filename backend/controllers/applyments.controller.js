4e08a1e53b45c35acf6474e7f6bb075d32f3b6f9
ChrisJara1415 <chris.jara0626@gmail.com>
2025-08-15 10:40:57 -0500

    Se crea el controlador de postulaciones

---
+++ backend/controllers/applyments.controller.js
@@ -0,0 +1,52 @@
+import applyments from '../models/applyments.model.js'
+
+export const createApplyment = async (req, res) => {
+    try {
+        const {servicio, empleado, fechaPostulacion, estado} = req.body
+        const nuevaPostulacion = new applyments({servicio, empleado, fechaPostulacion, estado})
+        await nuevaPostulacion.save()
+
+        res.status(201).json({message: 'Postulación creada satisfactoriamente', data: nuevaPostulacion})
+    } catch (error) {
+        res.status(500).json({message: 'Error al crear postulación'})
+    }
+}
+
+export const searchApplyments = async (req, res) => {
+    try {
+        const postulacionesEncontradas = await applyments.find()
+        res.status(200).json(postulacionesEncontradas)
+    } catch (error) {
+        res.status(500).json({message: 'No se han encontrado postulaciones', error})
+    }
+}
+
+export const searchApplymentById = async (req, res) => {
+    try {
+        const postulacionEncontrada = await applyments.findById(req.params.id)
+        if (!postulacionEncontrada) return res.status(404).json({message: 'No se ha encontrado la postulación'})
+        res.status(200).json(postulacionEncontrada)
+    } catch (error) {
+        res.status(500).json({message: 'No se ha encontrado ninguna postulación', error})
+    }
+}
+
+export const updateApplyment = async (req, res) => {
+    try {
+        const postulacionActualizada = await applyments.findByIdAndUpdate(req.param.id, req.body, {new: true})
+        if (!postulacionActualizada) return res.status(404).json({message: 'No se ha encontrado la postulación'})
+        res.status(200).json(postulacionActualizada)
+    } catch (error) {
+        res.status(500).json({message: 'No se ha encontrado ninguna postulación', error})
+    }
+}
+
+export const deleteApplyment = async (req, res) => {
+    try {
+        const postulacionEliminada = await applyments.findByIdAndDelete(req.params.id)
+        if (!postulacionEliminada) return res.status(404).json({message: 'Postulación no encontrada'})
+        res.status(200).json({message: 'Postulación eliminada satisfactoriamente'})
+    } catch (error) {
+        res.status(500).json({message: 'No se ha encontrado ninguna postulación', error})
+    }
+}
