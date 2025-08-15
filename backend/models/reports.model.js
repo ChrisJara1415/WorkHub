36a1aa25a9700ea34ca278fb2790d9a6ced89f0a
ChrisJara1415 <chris.jara0626@gmail.com>
2025-08-15 13:21:51 -0500

    Se cambia el import de mongoose a que apunten a la configuración de la base de datos, y algunos archivos son renombrados a plural

--- backend/models/reports.model.js
+++ backend/models/reports.model.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose'
+import mongoose from '../config/db.js'
 
 const { Schema } = mongoose
 const reportSchema = new Schema({
--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose'
+import mongoose from '../config/db.js'
 
 const schema = mongoose.Schema
 const userSchema = new schema({
