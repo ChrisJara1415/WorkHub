07bf5b10bceb5ca37226502ce85ddc974159084e
ChrisJara1415 <chris.jara0626@gmail.com>
2025-08-22 18:02:24 -0500

    Se agrega la validación de Api Key para ciertas acciones

---
+++ backend/middlewares/api.key.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv"
+dotenv.config()
+
+const validateApiKey = (req, res, next) => {
+    const apiKey = req.headers['x-api-key']
+    const validApiKey = process.env.API_KEY
+    console.log(process.env.API_KEY)
+
+    if (!apiKey) return res.status(401).json({error: 'API Key requerida', mensaje: 'Debe proporcionar una Api Key en el header'})
+    if (apiKey !== validApiKey) return res.status(401).json({error: 'API Key inválida', mensaje: 'La API Key proporcionada no es válida'})
+
+    next()
+}
+
+export default validateApiKey
