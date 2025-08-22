3b6ea0f75b1f00347fd3331cac67bb9ab5958af8
ChrisJara1415 <chris.jara0626@gmail.com>
2025-08-22 18:44:56 -0500

    Se crean las rutas globales para no llenar el index.js de rutas y también para una mejor gestión en caso de que se agreguen más endpoints

---
+++ backend/routers/globalRoutes.router.js
@@ -0,0 +1,16 @@
+import usersRouter from './users.router.js'
+import reportsRouter from './reports.router.js'
+import offersRouter from './offers.router.js'
+import contractsRouter from './contracts.router.js'
+import applymentsRouter from './applyments.router.js'
+import { Router } from 'express'
+
+const router = Router()
+
+router.use('/clientes', usersRouter)
+router.use('/reportes', reportsRouter)
+router.use('/ofertas', offersRouter)
+router.use('/contratos', contractsRouter)
+router.use('/postulaciones', applymentsRouter)
+
+export default router;
