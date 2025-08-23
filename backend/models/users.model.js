aa2239fa9aa979a84d4fb5ec27e4f22b16845cd3
ChrisJara1415 <chris.jara0626@gmail.com>
2025-08-22 19:37:35 -0500

    Se mejora la validación de fechas

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -56,10 +56,13 @@ const userSchema = new schema({
         default: Date.now,
         validate: {
             validator: function (value) {
-                const date = new Date();
-                return value >= date;
+                const now = new Date();
+                const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+                const maxDate = new Date(today);
+                maxDate.setMonth(maxDate.getMonth() + 1); 
+                return value >= today && value <= maxDate;
             },
-            message: props => `${props.value} no puede ser una fecha anterior a hoy!`
+            message: props => `${props.value} debe estar entre hoy y máximo 1 mes en el futuro.`
         }
     },
 
