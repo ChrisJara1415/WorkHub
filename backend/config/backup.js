4786d00a2231568d9f93721733999c0e00125cc3
ChrisJara1415 <chris.jara0626@gmail.com>
2025-09-06 21:47:24 -0500

    Se crea el script para generar backups de la base de datos

---
+++ backend/config/backup.js
@@ -0,0 +1,17 @@
+import { exec } from "node:child_process";
+process.loadEnvFile('.env')
+
+export async function backupDatabase() {
+    // const dbName = process.env.NAME_DB;
+    const outputPath = './backup';
+
+    const command = `mongodump --uri "${process.env.MONGO_URI}" --out ${outputPath} --gzip`;
+
+    await exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error en el respaldo: ${error.message}`);
+            return;
+        }
+        console.log(`Respaldo completado con éxito ${stdout}`);
+    });
+}
