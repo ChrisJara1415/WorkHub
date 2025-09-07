866edc43c753962543f23c481d0e1eb52f99e4e7
ChrisJara1415 <chris.jara0626@gmail.com>
2025-09-06 23:37:33 -0500

    Se importa la dependencia de express-ejs-layouts en index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,5 @@
 import express from "express"
+import expLayouts from "express-ejs-layouts"
 import path from "path"
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
@@ -18,6 +19,7 @@ app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(methodOverride("_method"))
+app.use(expLayouts)
 
 // Archivos estáticos
 app.use(express.static(path.join(__dirname, "static")))
