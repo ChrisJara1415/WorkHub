f96295bf4a164db4673a8a279530a405326e9088
ChrisJara1415 <chris.jara0626@gmail.com>
2025-09-14 10:57:22 -0500

    Como se movió el .env de lugar se cambia la manera en la que se llama las variables de entorno

--- backend/config/backup.js
+++ backend/config/backup.js
@@ -1,12 +1,12 @@
 import { exec } from "node:child_process";
-process.loadEnvFile()
+process.loadEnvFile('../.env')
+const MONGO_URI = process.env.MONGO_URI;
+
 export async function backupDatabase() {
-    // const dbName = process.env.NAME_DB;
     const outputPath = './backup';
+    const command = `mongodump --uri "${MONGO_URI}" --out ${outputPath} --gzip`;
 
-    const command = `mongodump --uri "${process.env.MONGO_URI}" --out ${outputPath} --gzip`;
-
-    await exec(command, (error, stdout, stderr) => {
+    exec(command, (error, stdout, _stderr) => {
         if (error) {
             console.error(`Error en el respaldo: ${error.message}`);
             return;
