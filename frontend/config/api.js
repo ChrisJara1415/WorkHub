a499e01bec2b3a9c4625bdd16f74a749bedd6f51
ChrisJara1415 <chris.jara0626@gmail.com>
2025-09-14 13:39:47 -0500

    Se cambian el nombre de las variables de entorno y la ruta de dónde se toman las variables

--- frontend/config/api.js
+++ frontend/config/api.js
@@ -1,13 +1,13 @@
 import axios from "axios"
-process.loadEnvFile()
+process.loadEnvFile('../.env')
 
-const API_BASE_URL = process.env.API_BASE_URL
+const BACK_URL = process.env.BACK_URL
 
 // Crear una instancia de axios con la configuración base
 // Esto permite reutilizar la configuración en diferentes rutas
 // sin la necesidad de repetir el código
 const apiClient = axios.create({
-  baseURL: API_BASE_URL, // Base URL de la API
+  baseURL: BACK_URL, // Base URL de la API
   timeout: 10000, // Tiempo máximo de espera para las solicitudes, en caso de tardar más de 10 segundos, se cancelará la solicitud
   headers: {
     "Content-Type": "application/json", // Tipo de contenido que se enviará en las solicitudes
