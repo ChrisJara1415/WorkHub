6f95e81aa55203dd403594a0735adf391f4ee07f
ChrisJara1415 <chris.jara0626@gmail.com>
2025-09-19 23:47:49 -0500

    Se agrega el endpoint de registro para que renderice el ejs respectivo

--- frontend/routes/indexRouter.js
+++ frontend/routes/indexRouter.js
@@ -3,5 +3,8 @@ const router = express.Router()
 import { renderLandingPage } from "../controllers/indexControllers/landingPageController.js";
 
 router.get('/', renderLandingPage);
+router.get('/registro', (req, res) => {
+	res.render('pages/register', { title: 'Registro | WorkHub', layout: false })
+});
 
 export default router
