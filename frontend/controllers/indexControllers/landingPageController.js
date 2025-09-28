03f8aa69fc461ecdc523e43b549f535738d3e81f
ChrisJara1415 <chris.jara0626@gmail.com>
2025-09-28 01:12:39 -0500

    Se agrega si está autenticado, la infor de usuario y rol para la información mostrada en la landing

--- frontend/controllers/indexControllers/landingPageController.js
+++ frontend/controllers/indexControllers/landingPageController.js
@@ -2,7 +2,10 @@ const renderLandingPage = async (req, res) => {
     try {
         res.render("pages/landingPage", {
             title: "Work Hub - Landing Page",
-            layout: false
+            layout: false,
+            user: res.locals.user,
+            isAuthenticated: res.locals.isAuthenticated,
+            role: res.locals.role
     })
     } catch (error) {
         console.error('Error al renderizar la página de inicio:', error)
